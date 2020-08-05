ac8aa6d37703050d30147233ef6ecb78e14f4099
sternenseemann <sternenseemann@systemli.org>
2020-08-05 14:41:17 +0200

    feat(xml): add library for outputting XML/HTML
    Currently this library is unused, but it will be utilized in an
    upcoming refactoring/rewrite of sternenblog…
    
    * … to construct the RSS feed
    * … to construct the HTML output of template/simple.c
    
    Documentation can be generated using doxygen.

---
+++ xml.c
@@ -0,0 +1,226 @@
+// TODO indent, html escaping
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "xml.h"
+
+void debug_xml_stack(FILE *out, struct xml_stack *stack) {
+    if(stack != NULL) {
+        fprintf(out, "%s ", stack->tag);
+        debug_xml_stack(out, stack->next);
+    } else {
+        fputc('\n', out);
+    }
+}
+
+void free_xml_stack(struct xml_stack *stack) {
+    if(stack == NULL) {
+        return;
+    }
+
+    if(stack->tag != NULL) {
+        free(stack->tag);
+    }
+
+    if(stack->next != NULL) {
+        free_xml_stack(stack->next);
+    }
+
+    free(stack);
+}
+
+void new_xml_context(struct xml_context *ctx) {
+    ctx->stack = NULL;
+    ctx->warn = NULL;
+    ctx->out = stdout;
+    ctx->closing_slash = 1;
+}
+
+void del_xml_context(struct xml_context *ctx) {
+    if(ctx->stack != NULL) {
+        if(ctx->warn != NULL) {
+            fputs("Unclosed tags remaining: ", ctx->warn);
+            debug_xml_stack(ctx->warn, ctx->stack);
+        }
+
+        free_xml_stack(ctx->stack);
+    }
+}
+
+void xml_raw(struct xml_context *ctx, const char *str) {
+    fputs(str, ctx->out);
+}
+
+void output_attrs(FILE *out, va_list attrs, size_t arg_count) {
+    if(arg_count > 0) {
+        for(int i = 1; i<=arg_count; i++) {
+            if(i % 2) {
+                char *name = va_arg(attrs, char *);
+                if(name == NULL) {
+                    break;
+                }
+
+                fputc(' ', out);
+                fputs(name, out);
+            } else {
+                char *maybe_val = va_arg(attrs, char *);
+                if(maybe_val != NULL) {
+                    fputs("=\"", out);
+                    fputs(maybe_val, out);
+                    fputc('\"', out);
+                }
+            }
+        }
+    }
+}
+
+void xml_empty_tag(struct xml_context *ctx, const char *tag, size_t attr_count, ...) {
+    if(tag == NULL || ctx == NULL) {
+        if(ctx->warn != NULL) {
+            fputs("Got no tag or ctx\n", ctx->warn);
+        }
+        return;
+    }
+
+    fputc('<', ctx->out);
+    fputs(tag, ctx->out);
+
+    if(attr_count > 0) {
+        size_t arg_count = attr_count * 2;
+
+        va_list attrs;
+        va_start(attrs, attr_count);
+
+        output_attrs(ctx->out, attrs, arg_count);
+
+        va_end(attrs);
+    }
+
+    if(ctx->closing_slash) {
+        fputc('/', ctx->out);
+    }
+
+    fputc('>', ctx->out);
+}
+
+void xml_open_tag_attrs(struct xml_context *ctx, const char *tag, size_t attr_count, ...) {
+    if(tag == NULL || ctx == NULL) {
+        if(ctx->warn != NULL) {
+            fputs("Got no tag or ctx\n", ctx->warn);
+        }
+        return;
+    }
+
+    struct xml_stack *old_stack = ctx->stack;
+
+    fputc('<', ctx->out);
+    fputs(tag, ctx->out);
+
+
+    if(attr_count > 0) {
+        size_t arg_count = attr_count * 2;
+
+        va_list attrs;
+        va_start(attrs, attr_count);
+
+        output_attrs(ctx->out, attrs, arg_count);
+
+        va_end(attrs);
+    }
+
+    fputc('>', ctx->out);
+
+    ctx->stack = malloc(sizeof(struct xml_context));
+
+    if(ctx->stack == NULL) {
+        if(ctx->warn != NULL) {
+            fputs("Could not allocate memory for tag stack, now everything will break.\n", ctx->warn);
+        }
+        return;
+    }
+
+    ctx->stack->next = old_stack;
+
+    size_t tag_size = strlen(tag) + 1;
+    ctx->stack->tag = malloc(sizeof(char) * tag_size);
+    memcpy(ctx->stack->tag, tag, tag_size);
+}
+
+void xml_open_tag(struct xml_context *ctx, const char *tag) {
+    xml_open_tag_attrs(ctx, tag, 0);
+}
+
+void xml_close_tag(struct xml_context *ctx, const char *tag) {
+    if(tag == NULL || ctx == NULL) {
+        if(ctx->warn != NULL) {
+            fputs("Got no tag or ctx\n", ctx->warn);
+        }
+        return;
+    }
+
+    if(ctx->stack == NULL || strcmp(tag, ctx->stack->tag) != 0) {
+        if(ctx->warn != NULL) {
+            fprintf(ctx->warn, "Refusing to close tag %s, ", tag);
+            if(ctx->stack == NULL) {
+                fputs("no tags left to be closed\n", ctx->warn);
+            } else {
+                fputs("unclosed tags remaining\n", ctx->warn);
+            }
+        }
+        return;
+    }
+
+    fputs("</", ctx->out);
+    fputs(tag, ctx->out);
+    fputc('>', ctx->out);
+
+    struct xml_stack *old_head = ctx->stack;
+
+    ctx->stack = old_head->next;
+
+    free(old_head->tag);
+    free(old_head);
+}
+
+void xml_close_all(struct xml_context *ctx) {
+    if(ctx == NULL) {
+        if(ctx->warn != NULL) {
+            fputs("Got no ctx\n", ctx->warn);
+        }
+        return;
+    }
+
+    if(ctx->stack == NULL) {
+        return;
+    } else {
+        xml_close_tag(ctx, ctx->stack->tag);
+        xml_close_all(ctx);
+    }
+}
+
+void xml_close_including(struct xml_context *ctx, const char *tag) {
+    if(ctx == NULL) {
+        if(ctx->warn != NULL) {
+            fputs("Got no ctx\n", ctx->warn);
+        }
+        return;
+    }
+
+    if(ctx->stack == NULL) {
+        if(ctx->warn != NULL) {
+            fprintf(ctx->warn, "Hit end of tag stack while searching for tag %s to close\n", tag);
+        }
+        return;
+    } else {
+        int last_tag = strcmp(tag, ctx->stack->tag) == 0;
+
+        xml_close_tag(ctx, ctx->stack->tag);
+
+        if(!last_tag) {
+            xml_close_including(ctx, tag);
+        }
+    }
+}
---
+++ xml.h
@@ -0,0 +1,311 @@
+/*!
+ * @file xml.h
+ * @brief Simple library for constructing XML documents
+ *
+ * This library provides a C interface for opening and closing
+ * XML tags as well as filling them with content. It is mainly
+ * intended for constructing XML/HTML documents by directly
+ * writing them to `stdout`.
+ *
+ * It's main advantage over plain `printf()` is that it keeps
+ * track of open tags, enabling it to automatically close
+ * open tags (saving a few lines of code) using `xml_close_all()`
+ * and `xml_close_including()` and/or to detect errors in the
+ * programmer's XML nesting. For information on its sanity
+ * checking abilities see the documentation of `xml_close_tag()`.
+ *
+ * Currently it has some limitations:
+ *
+ * * It does not give the calling code feedback if errors occurred
+ * * It can't do any type of escaping (neither in attributes nor
+ *   in content of XML tags)
+ * * It doesn't do validity checking of tags and attributes
+ *   (legal characters etc.)
+ * * It can't generate pretty output (i. e. properly indented),
+ *   its output is currently always "minified".
+ * * It currently has no satisfying support for `CDATA`,
+ *   XML declarations and probably a lot more
+ *
+ * For handling arbitrary data this library is probably not a good
+ * fit, it is mainly intended and tested for generating HTML and
+ * RSS documents in a CGI-like environment from trusted data.
+ *
+ * An example application generating a HTML5 page looks like this:
+ *
+ * ```c
+ * #include "xml.h"
+ *
+ * int main() {
+ *     struct xml_context ctx;
+ *     new_xml_context(&ctx);
+ *
+ *     ctx.closing_slash = 0;
+ *
+ *     xml_raw(&ctx, "<!doctype html>");
+ *     xml_open_tag_attrs(&ctx, "html", 1, "lang", "en");
+ *     xml_open_tag(&ctx, "head");
+ *     xml_empty_tag(&ctx, "meta", 1, "charset", "utf-8");
+ *     xml_open_tag(&ctx, "title");
+ *     xml_raw(&ctx, "lol this is my site");
+ *     xml_close_including(&ctx, "head");
+ *     xml_open_tag(&ctx, "body");
+ *     xml_open_tag(&ctx, "header");
+ *     xml_open_tag(&ctx, "h1");
+ *     xml_raw(&ctx, "sophisticated technology");
+ *     xml_close_including(&ctx, "header");
+ *     xml_open_tag(&ctx, "main");
+ *     xml_open_tag(&ctx, "article");
+ *     xml_open_tag(&ctx, "h2");
+ *     xml_raw(&ctx, "i was joking");
+ *     xml_close_tag(&ctx, "h2");
+ *     xml_open_tag(&ctx, "p");
+ *     xml_raw(&ctx, "it really was only a joke, i was leading you on!");
+ *     xml_close_including(&ctx, "article");
+ *     xml_open_tag(&ctx, "article");
+ *     xml_open_tag(&ctx, "pre");
+ *     xml_raw(&ctx, "+-----+--------------------+\n");
+ *     xml_raw(&ctx, "| wow | this aligns right! |\n");
+ *     xml_raw(&ctx, "+-----+--------------------+\n");
+ *     xml_close_all(&ctx);
+ *
+ *     del_xml_context(&ctx);
+ *     return 0;
+ * }
+ * ```
+ *
+ */
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+/*!
+ * @brief Internal linked list type
+ *
+ * Linked list used internally to keep track of tags to close.
+ *
+ * @see struct xml_context
+ */
+struct xml_stack {
+    char *tag;               //!< tag name
+    struct xml_stack *next;  //!< tag to be closed after the current one
+};
+
+/*!
+ * @brief State and configuration of xml generation.
+ *
+ * Struct containing both state and configuration of this module.
+ * See `new_xml_context()` for usage instructions.
+ *
+ * @see new_xml_context
+ * @see del_xml_context
+ * @see struct xml_stack
+ */
+struct xml_context {
+    struct xml_stack *stack; //!< linked list used internally to keep track of open tags
+    FILE *out;               //!< Where to write output, defaults to stdout
+    FILE *warn;              //!< if not `NULL`, print warnings to handle warn, defaults to `NULL`
+    bool closing_slash;      //!< whether to output a closing slash at the end of an empty tag
+};
+
+/*!
+ * @brief Initialize the `xml_context` structure.
+ *
+ * Initialize a `struct xml_context` with default values:
+ *
+ * * empty stack
+ * * output to `stdout`
+ * * no warnings
+ * * closing slashes enabled
+ *
+ * This function should always be called before any other functions of xml.
+ * If you want to use different settings than the default ones, update the
+ * struct after calling this function. This way your application won't break
+ * if it gets extended.
+ *
+ * @see struct xml_context
+ * @see del_xml_context
+ */
+void new_xml_context(struct xml_context *ctx);
+
+/*!
+ * @brief Clean up the `xml_context` structure.
+ *
+ * Frees any dynamically allocated data in a `struct xml_context`.
+ * Should always be called before a `struct xml_context` goes out
+ * of scope or the program terminates.
+ *
+ * If `ctx->warn` is not `NULL`, `del_xml_context()` will additionally
+ * output a message about any remaining unclosed tags to `ctx->warn`.
+ * For example:
+ *
+ * ```
+ * Unclosed tags remaining: article main body html
+ * ```
+ *
+ * @see struct xml_context
+ * @see new_xml_context
+ */
+void del_xml_context(struct xml_context *ctx);
+
+/*!
+ * @brief Output a raw string.
+ *
+ * Output string to `ctx->out`, equivalent to `fputs(str, ctx.out)`.
+ * Note that any escaping necessary must be done manually.
+ *
+ * @see struct xml_context
+ */
+void xml_raw(struct xml_context *ctx, const char *str);
+
+/*!
+ * @brief Output an empty xml tag.
+ *
+ * Output an empty xml tag (i. e. a single tag that doesn't need to be closed).
+ * This call does not change the provided context `ctx`.
+ *
+ * The call also outputs given attributes: For `attr_count` n, `xml_empty_tag()` expects
+ * 2n additional arguments — for each attribute a name and a value. If value is `NULL`,
+ * an attribute without a value will be output, i. e. just the name without the `="…"`.
+ *
+ * For example, `xml_empty_tag(&ctx, "my-tag", 2, "foo", "bar", "baz", NULL);` gives
+ * `<my-tag foo="bar" baz/>` with default settings.
+ *
+ * If `closing_slash` is 0 in `ctx`, the slash before the closing ">" will be omitted.
+ * This is useful for HTML5 where it is optional.
+ *
+ * @see struct xml_context
+ */
+void xml_empty_tag(struct xml_context *ctx, const char *tag, size_t attr_count, ...);
+
+/*!
+ * @brief Output an opening tag with attributes.
+ *
+ * Output an opening tag with attributes and add it to `ctx->stack` for future reference.
+ *
+ * Attributes work exactly like in `xml_empty_tag()`.
+ * @see xml_empty_tag
+ */
+void xml_open_tag_attrs(struct xml_context *ctx, const char *tag, size_t attr_count, ...);
+
+/*!
+ * @brief Output an opening tag without any attributes.
+ *
+ * Shorthand for `xml_open_tag_attrs(ctx, tag, 0)`.
+ * @see xml_open_tag_attrs
+ */
+void xml_open_tag(struct xml_context *ctx, const char *tag);
+
+/*!
+ * @brief Close a previously opened tag.
+ *
+ * `xml_close_tag()` first checks the head of the current `xml_stack`
+ * if the provided `tag` is in fact the current innermost opened tag.
+ *
+ * If this is true, it outputs the closing tag, removes the reference
+ * to the tag on top of the `xml_stack` and frees this part of the
+ * structure.
+ *
+ * If it isn't true, it does nothing and outputs an appropriate warning
+ * to `ctx->warn` if it is not `NULL`:
+ *
+ * * `Refusing to close tag xyz, unclosed tags remaining`
+ * * `Refusing to close tag zyx, no tags left to be closed`
+ *
+ * This sanity checking of tag closing ensures that a xml document
+ * constructed by this module has no nesting errors, i. e. every tag
+ * is closed at the proper nesting level. Because it is only simple
+ * runtime checking in specific calls it can't prevent / detect the
+ * following errors:
+ *
+ * * It can't prevent unclosed tags remaining at the end. You can
+ *   however prevent this by calling `xml_close_all()` at the end of
+ *   your XML outputting code.
+ * * It can worsen a situation with remaining unclosed tags: If
+ *   an inner tag is left unclosed, it will refuse to close all
+ *   outer tags, leaving a trail of unclosed tags behind.
+ * * It will always attribute the error to closing: Some errors
+ *   will be caused by missing an `xml_open_tag()` somewhere, but
+ *   `xml_close_tag()` will think the closing is erroneous. Of course
+ *   it's also unable to resolve the error.
+ * * It can't compare against the intended XML structure: Sometimes
+ *   a programming error will result in an "wrong" XML structure
+ *   which is still completely valid to `xml_close_tag()`, i. e.
+ *   correctly nested.
+ *
+ * Overall the sanity checking is limited, as you can see, but
+ * it should generate *some* warning if a detectable issue is
+ * present (invalid XML nesting) and never actively output
+ * a tag that makes the XML nesting invalid.
+ *
+ * These properties should however be enough to detect issues
+ * quickly in development. Additionally the sanity checking is
+ * cheap enough to be feasible in production. `xml_close_tag()`
+ * only needs to call `strcmp` once per invocation.
+ *
+ * @see xml_open_tag_attrs
+ * @see xml_open_tag
+ * @see struct xml_stack
+ * @see xml_close_all
+ * @see xml_close_including
+ */
+void xml_close_tag(struct xml_context *ctx, const char *tag);
+
+/*!
+ * @brief Close all remaining unclosed tags
+ *
+ * `xml_close_all()` iterates through the `xml_stack` and calls
+ * `xml_close_tag()` for every tag in it. A call to it will thus
+ * result in an empty `xml_stack` and all previously opened tags
+ * being closed correctly.
+ *
+ * Note that `xml_close_all()` will limit error checking, since it
+ * (by nature) always succeeds and has no insight into what the
+ * programmer thinks needs to be closed.
+ *
+ * @see xml_close_tag
+ * @see xml_close_including
+ * @see struct xml_stack
+ */
+void xml_close_all(struct xml_context *ctx);
+
+/*!
+ * @brief Close all unclosed tags until a given one.
+ *
+ * `xml_close_including()` works like `xml_close_all()`, but
+ * will stop after it hits a tag of the given name.
+ * If the given tag is not present in the stack, it behaves
+ * like `xml_close_all()`.
+ *
+ * Be aware that it might lead to unexpected results if
+ * multiple tags of the same are nested. Consider the
+ * following snippet.
+ *
+ * ```c
+ * xml_open_tag(&ctx, "a");
+ * xml_open_tag(&ctx, "b");
+ * xml_close_tag(&ctx, "a");
+ * xml_open_tag(&ctx, "c");
+ * xml_raw(&ctx, "value");
+ * xml_close_including(&ctx, "a");
+ * ```
+ *
+ * `xml_close_including()` will stop as soon as it hits the first
+ * tag "a", although it might be intended to keep going until the
+ * outermost one. The result would be:
+ *
+ * ```xml
+ * <a><b><a><c>value</c></a>
+ * ```
+ *
+ * This is the behavior of `xml_close_including()`: It closes all
+ * unclosed tags until it hits the first instance of the specified
+ * tag which is also closed.
+ *
+ * This function will also limit error detection like `xml_close_all()`.
+ * For an explanation of this, see its documentation.
+ *
+ * @see xml_close_all
+ * @see xml_close_tag
+ * @see struct xml_stack
+ */
+void xml_close_including(struct xml_context *ctx, const char *tag);
